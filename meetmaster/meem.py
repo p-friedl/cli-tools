847181a4df93d2b02ea8ab1a4aff46873ccb73c3
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-03 01:25:50 +0100

    saved bug in load / save, added more item types

--- meetmaster/meem.py
+++ meetmaster/meem.py
@@ -23,50 +23,50 @@ class Protocol:
         self.items = []
 
     def save(self):
-        with open('protocol.csv', mode='w') as csv_file:
+        with open("protocol.csv", mode="w") as csv_file:
             csv_file.truncate()
-            fieldnames = ['creation_date', 'creation_time', 'type', 'title', 'desc', 'given_by', 'result', 'owner',
-                          'priority', 'due']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter='|')
+            fieldnames = ["creation_date", "creation_time", "type", "title", "desc", "given_by", "result", "owner",
+                          "priority", "due"]
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter="|")
             writer.writeheader()
 
             for item in self.items:
-                row = {'creation_date': item.creation_date,
-                       'creation_time': item.creation_time,
-                       'type': item.type,
-                       'title': item.title,
-                       'desc': item.desc}
+                row = {"creation_date": item.creation_date,
+                       "creation_time": item.creation_time,
+                       "type": item.type,
+                       "title": item.title,
+                       "desc": item.desc}
 
-                if item.type == 'information':
-                    row['given_by'] = item.given_by
-                elif item.type == 'decision':
-                    row['result'] = item.result
-                elif item.type == 'task':
-                    row['owner'] = item.owner
-                    row['priority'] = item.priority
-                    row['due'] = item.due
+                if item.type == "Information":
+                    row["given_by"] = item.given_by
+                elif item.type == "Decision":
+                    row["result"] = item.result
+                elif item.type == "Task":
+                    row["owner"] = item.owner
+                    row["priority"] = item.priority
+                    row["due"] = item.due
 
                 writer.writerow(row)
 
     def load(self):
         try:
-            with open('protocol.csv', mode='r') as csv_file:
-                csv_reader = csv.DictReader(csv_file, delimiter='|')
+            with open("protocol.csv", mode="r") as csv_file:
+                csv_reader = csv.DictReader(csv_file, delimiter="|")
 
                 for index, row in enumerate(csv_reader):
-                    if index != 0:
-                        if row['type'] == 'information':
-                            item = Information(row['title'], row['desc'], row['given_by'])
-                        if row['type'] == 'decision':
-                            item = Decision(row['title'], row['desc'], row['result'])
-                        if row['type'] == 'task':
-                            item = Task(row['title'], row['desc'], row['owner'], row['priority'], row['due'])
-                        item.creation_date = row['creation_date']
-                        item.creation_time = row['creation_time']
+                    if index != "0":
+                        if row["type"] == "Information":
+                            item = Information(row["title"], row["desc"], row["given_by"])
+                        if row["type"] == "Decision":
+                            item = Decision(row["title"], row["desc"], row["result"])
+                        if row["type"] == "Task":
+                            item = Task(row["title"], row["desc"], row["owner"], row["priority"], row["due"])
+                        item.creation_date = row["creation_date"]
+                        item.creation_time = row["creation_time"]
                         self.add_item(item)
         except IOError:
             # If not exists, create the file
-            csv_file = open('protocol.csv', 'w+')
+            csv_file = open("protocol.csv", "w+")
             csv_file.close()
 
 
@@ -115,20 +115,34 @@ def show():
 
 
 @cli.command()
-@click.option('-i', 'item_type', flag_value='information')
-@click.option('-d', 'item_type', flag_value='decision')
-@click.option('-t', 'item_type', flag_value='task')
-@click.argument('title')
+@click.option("-i", "item_type", flag_value="Information")
+@click.option("-d", "item_type", flag_value="Decision")
+@click.option("-t", "item_type", flag_value="Task")
+@click.argument("title", nargs=-1)
 def add(item_type, title):
-    protocol = Protocol()
-    protocol.load()
-    if item_type == "information":
-        desc = click.prompt('Description', type=str)
-        given_by = click.prompt('Information Provider', type=str)
-        item = Information(title, desc, given_by)
+    if item_type:
+        protocol = Protocol()
+        protocol.load()
+        title = " ".join(title)
+        desc = click.prompt("Description", type=str)
+
+        if item_type == "Information":
+            given_by = click.prompt("Information Provider", type=str)
+            item = Information(title, desc, given_by)
+        elif item_type == "Decision":
+            result = click.prompt("Decision Result", type=str)
+            item = Decision(title, desc, result)
+        elif item_type == "Task":
+            owner = click.prompt("Task Owner", type=str)
+            priority = click.prompt("Task Priority", type=str)
+            due = click.prompt("Task Due Date", type=str)
+            item = Task(title, desc, owner, priority, due)
+
         protocol.add_item(item)
-    protocol.save()
+        protocol.save()
+    else:
+        click.echo("Invalid: please provide a valid option parameter")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
