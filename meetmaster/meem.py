2cb4c626ca02af84512a30e004ee0181c63988ac
Patrik Friedl <patrik.friedl@gmail.com>
2019-11-03 21:28:01 +0100

    added commands to list items based on type, remove items and delete the whole protocol

--- meetmaster/meem.py
+++ meetmaster/meem.py
@@ -4,25 +4,51 @@ import csv
 
 
 class Protocol:
+    """
+    Protocol class that contains the protocol items
+    """
     def __init__(self):
         self.items = []
 
     def list_items(self):
-        return [item.title for item in self.items]
+        """
+        List all protocol items
+        """
+        click.echo("ID --|-- Item Title")
+        for index, item in enumerate(self.items):
+            click.echo(" {} --|-- {}".format(index, item.title))
 
     def list_items_by_type(self, item_type):
-        return [item.title for item in self.items if item.type == item_type]
+        """
+        List protocol items by a specific type
+        """
+        click.echo("ID --|-- Item Title")
+        for index, item in enumerate(self.items):
+            if item.type.lower() == item_type.lower():
+                click.echo(" {} --|-- {}".format(index, item.title))
 
     def add_item(self, item):
+        """
+        add a protocol item
+        """
         self.items.append(item)
 
     def remove_item(self, item_id):
+        """
+        remove a protocol item
+        """
         self.items.pop(item_id)
 
     def clear(self):
+        """
+        delete the protocol
+        """
         self.items = []
 
     def save(self):
+        """
+        save the protocol to a CSV file
+        """
         with open("protocol.csv", mode="w") as csv_file:
             csv_file.truncate()
             fieldnames = ["creation_date", "creation_time", "type", "title", "desc", "given_by", "result", "owner",
@@ -49,6 +75,9 @@ class Protocol:
                 writer.writerow(row)
 
     def load(self):
+        """
+        load the protocol from a CSV file
+        """
         try:
             with open("protocol.csv", mode="r") as csv_file:
                 csv_reader = csv.DictReader(csv_file, delimiter="|")
@@ -65,12 +94,15 @@ class Protocol:
                         item.creation_time = row["creation_time"]
                         self.add_item(item)
         except IOError:
-            # If not exists, create the file
+            # create the file if it doesn't exist yet
             csv_file = open("protocol.csv", "w+")
             csv_file.close()
 
 
 class ProtocolItem:
+    """
+    Protocol item base class
+    """
     def __init__(self, title, desc):
         timestamp = datetime.now()
         self.creation_date = "{}/{}/{}".format(timestamp.day, timestamp.month, timestamp.year)
@@ -80,6 +112,9 @@ class ProtocolItem:
 
 
 class Information(ProtocolItem):
+    """
+    Protocol item class of type Information
+    """
     def __init__(self, title, desc, given_by):
         super().__init__(title, desc)
         self.given_by = given_by
@@ -87,6 +122,9 @@ class Information(ProtocolItem):
 
 
 class Decision(ProtocolItem):
+    """
+    Protocol item class of type Decision
+    """
     def __init__(self, title, desc, result):
         super().__init__(title, desc)
         self.result = result
@@ -94,6 +132,9 @@ class Decision(ProtocolItem):
 
 
 class Task(ProtocolItem):
+    """
+    Protocol item class of type Task
+    """
     def __init__(self, title, desc, owner, priority, due):
         super().__init__(title, desc)
         self.owner = owner
@@ -102,18 +143,25 @@ class Task(ProtocolItem):
         self.type = "Task"
 
 
+# Group CLI commands
 @click.group()
 def cli():
     pass
 
 
+# Command to show all protocol items or specific ones per type
 @cli.command()
-def show():
+@click.option("-t", "item_type", type=click.Choice(["information", "decision", "task"], case_sensitive=False))
+def show(item_type):
     protocol = Protocol()
     protocol.load()
-    click.echo(protocol.list_items())
+    if item_type:
+        protocol.list_items_by_type(item_type)
+    else:
+        protocol.list_items()
 
 
+# Command to add a new protocol item
 @cli.command()
 @click.option("-i", "item_type", flag_value="Information")
 @click.option("-d", "item_type", flag_value="Decision")
@@ -142,6 +190,30 @@ def add(item_type, title):
         protocol.save()
     else:
         click.echo("Invalid: please provide a valid option parameter")
+
+
+# Command to delete a specific protocol item by ID
+@cli.command()
+@click.confirmation_option(prompt="Are you sure you want to delete the chosen protocol item?")
+@click.argument("item_id", type=click.INT)
+def remove(item_id):
+    protocol = Protocol()
+    protocol.load()
+    if item_id > len(protocol.items) - 1:
+        click.echo("Invalid: the ID does not exist and therefore the item can't be deleted")
+    else:
+        protocol.remove_item(item_id)
+        protocol.save()
+
+
+# Command to delete the whole protocol
+@cli.command()
+@click.confirmation_option(prompt="Are you sure you want to delete the chosen protocol item?")
+def delete():
+    protocol = Protocol()
+    protocol.load()
+    protocol.clear()
+    protocol.save()
 
 
 if __name__ == "__main__":
